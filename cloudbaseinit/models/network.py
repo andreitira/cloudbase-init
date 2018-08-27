c841fa9eebea7bfd46ec4ba3685bff62ca3e5b1c
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2018-08-28 00:50:57 +0300

    Update model

--- cloudbaseinit/models/network.py
+++ cloudbaseinit/models/network.py
@@ -18,6 +18,7 @@ LINK_TYPE_PHYSICAL = "physical"
 LINK_TYPE_BOND = "bond"
 LINK_TYPE_VLAN = "vlan"
 
+# Based on: https://www.kernel.org/doc/Documentation/networking/bonding.txts
 BOND_TYPE_8023AD = "802.3ad"
 BOND_TYPE_BALANCE_RR = "balance-rr"
 BOND_TYPE_ACTIVE_BACKUP = "active-backup"
@@ -26,11 +27,29 @@ BOND_TYPE_BROADCAST = "broadcast"
 BOND_TYPE_BALANCE_TLB = "balance-tlb"
 BOND_TYPE_BALANCE_ALB = "balance-alb"
 
+AVAILABLE_BOND_TYPES = [
+    BOND_TYPE_8023AD,
+    BOND_TYPE_BALANCE_RR,
+    BOND_TYPE_ACTIVE_BACKUP,
+    BOND_TYPE_BALANCE_XOR,
+    BOND_TYPE_BROADCAST,
+    BOND_TYPE_BALANCE_TLB,
+    BOND_TYPE_BALANCE_ALB,
+]
+
 BOND_LB_ALGO_L2 = "layer2"
 BOND_LB_ALGO_L2_L3 = "layer2+3"
 BOND_LB_ALGO_L3_L4 = "layer3+4"
 BOND_LB_ENCAP_L2_L3 = "encap2+3"
 BOND_LB_ENCAP_L3_L4 = "encap3+4"
+
+AVAILABLE_BOND_LB_ALGORITHMS = [
+    BOND_LB_ALGO_L2,
+    BOND_LB_ALGO_L2_L3,
+    BOND_LB_ALGO_L3_L4,
+    BOND_LB_ENCAP_L2_L3,
+    BOND_LB_ENCAP_L3_L4,
+]
 
 BOND_LACP_RATE_SLOW = "slow"
 BOND_LACP_RATE_FAST = "fast"
